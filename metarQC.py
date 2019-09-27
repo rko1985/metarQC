16d8016090ac0ed4d8618e09c28f5e547b801ccc
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-09-27 07:40:23 -1000

    made improvements to LTG qc

--- metarQC.py
+++ metarQC.py
@@ -16,17 +16,18 @@ for site in sites:
     for metar in metars:
 
         if("RMK" in metar.text):
-            metarsplit = metar.text.split("RMK")
+            metarsplit = metar.text.split("RMK") #splitting before and after remarks
+            metarsplit[0] = " ".join(metarsplit[0].split()[1:]) #getting rid of station ID
         
         ##WARNINGS
-        '''if "CLR" in metar.text:        
-            print("Warning, CLR in metar " + metar.text)'''
+        'if "CLR" in metar.text:        
+            print("Warning, CLR in metar " + metar.text)
             
         if "TS" in metarsplit[0]:
             print("Warning, TS in metar, check further manually " + metar.text)
 
         ##ERRORS        
-        '''if "AUTO" in metar.text:        
+        if "AUTO" in metar.text:        
             print("ERROR AUTO in metar " + metar.text)
         
         if("RA" in metarsplit[0]):
@@ -99,6 +100,81 @@ for site in sites:
         #TS in present wx but no CB
         if "TS" in metarsplit[0]:
             if "CB" not in metarsplit[0]:
-                print("ERROR: TS in present WX, no CB in cloud layer " + metar.text)'''                
-    
+                print("ERROR: TS in present WX, no CB in cloud layer " + metar.text)
 
+
+        #LTG formatting
+        frequencies = ['FRQ', 'OCNL', 'CONS']
+        directions = ['N','NE','E','SE','S','SW','W','NW','UNKN','ALQDS']
+        ltg_errors = [0,0,0,0,0,0,0]
+                    
+        lightningDSNT = re.findall(r"\b(\w+\s)(LTG\w+\s)(\DSNT\s)(\S+\s(?:AND \w+\s)?)(\w+\s)(\w+\s)(\S+\s(?:AND \w+\s)?)\b", metarsplit[1])
+        lightningOHD = re.findall(r"\b(\w+\s)(LTG\w+\s)(\OHD\s)(\w+\s)(\w+\s)(\w+\s)(\S+)\b", metarsplit[1])
+        lightningVC = re.findall(r"\b(\w+\s)(LTG\w+\s)(\VC\s)(\S+\s(?:AND \w+\s)?)(\w+\s)(\w+\s)(\S+\s(?:AND \w+\s)?)(\w+\s)(\S+)\b", metarsplit[1])
+
+        
+        if "LTG" in metarsplit[1]:
+            #DSNT lighting[0] length should be 7
+            if(len(lightningDSNT) > 0):
+                if lightningDSNT[0][2].strip() == 'DSNT':
+                    for frequency in frequencies:
+                        if frequency == lightningDSNT[0][0].strip():                    
+                            ltg_errors[0] += 1
+                    for direction in directions:
+                        if direction in lightningDSNT[0][3].strip():                    
+                            ltg_errors[1] += 1
+                        if direction in lightningDSNT[0][6].strip():                    
+                            ltg_errors[2] += 1
+                    if lightningDSNT[0][4].strip() == 'CB':                
+                        ltg_errors[3] += 1
+                    if lightningDSNT[0][5].strip() == 'DSNT':                
+                        ltg_errors[4] += 1
+                if ltg_errors[0] == 0 or ltg_errors[1] == 0 or ltg_errors[2] == 0 or ltg_errors[3] == 0 or ltg_errors[4] == 0:
+                    print("Error in LTG remark " + metar.text)
+                    ltg_errors = [0,0,0,0,0,0,0]
+
+            #OHD lighting[0] length should be 7
+            if(len(lightningOHD) > 0):
+                if lightningOHD[0][2].strip() == 'OHD':
+                    for frequency in frequencies:
+                        if frequency == lightningOHD[0][0].strip():
+                            ltg_errors[0] += 1
+                    for direction in directions:
+                        if direction in lightningOHD[0][6].strip():
+                            ltg_errors[1] += 1
+                    if lightningOHD[0][3].strip() == 'TS':
+                        ltg_errors[2] += 1
+                    if lightningOHD[0][4].strip() == 'OHD':
+                        ltg_errors[3] += 1
+                    if lightningOHD[0][5].strip() == 'MOV':
+                        ltg_errors[4] += 1
+                if ltg_errors[0] == 0 or ltg_errors[1] == 0 or ltg_errors[2] == 0 or ltg_errors[3] == 0 or ltg_errors[4] == 0:
+                    print("Error in LTG remark " + metar.text)
+                    ltg_errors = [0,0,0,0,0,0,0]
+
+            #VC lightning[0] should be length 9
+            if(len(lightningVC) > 0):
+                if lightningVC[0][2].strip() == 'VC': #strip whitespace after VC
+                    for frequency in frequencies:
+                        if frequency == lightningVC[0][0].strip():
+                            ltg_errors[0] += 1                    
+                    for direction in directions:
+                        if direction in lightningVC[0][3].strip():
+                            ltg_errors[1] += 1
+                        if direction in lightningVC[0][6].strip():
+                            ltg_errors[2] += 1
+                        if direction in lightningVC[0][8].strip():
+                            ltg_errors[3] += 1                
+                    if lightningVC[0][4].strip() == 'TS':
+                        ltg_errors[4] += 1
+                    if lightningVC[0][5].strip() == 'VC':
+                        ltg_errors[5] += 1
+                    if lightningVC[0][7].strip() == 'MOV':
+                        ltg_errors[6] += 1            
+                if ltg_errors[0] == 0 or ltg_errors[1] == 0 or ltg_errors[2] == 0 or ltg_errors[3] == 0 or ltg_errors[4] == 0 or ltg_errors[5] == 0 or ltg_errors[6] == 0:
+                    print("Error in LTG remark " + metar.text)                    
+                    ltg_errors = [0,0,0,0,0,0,0]
+
+        if "LTG" in metarsplit[1]:
+            if len(lightningDSNT) == 0 and len(lightningOHD) == 0 and len(lightningVC) == 0:
+                print("Error in lightning remarks " + metar.text)
--- tsQC.py
+++ tsQC.py
@@ -1,73 +1,87 @@
 import re
 
+metar = "KLIT 260053Z 16006KT 10SM SCT120 26/19 A2985 RMK AO2 SLP107 OCNL LTGIC DSNT NE-W AND W CB DSNT NE T02560189"
+
+if("RMK" in metar):
+    metarsplit = metar.split("RMK")
+
 frequencies = ['FRQ', 'OCNL', 'CONS']
-directions = ['N','NE','E','SE','S','SW','W','NW','UNKN']
-
-metar = "KLIT 231847Z 15008KT 10SM -TSRA FEW015 BKN030CB OVC100 26/23 A3004 RMK AO2 RAE14B46 TSB05E27B46 OCNL LTGICCC DSNT W-N CB DSNT W-N P0000"
-
-
-lightningDSNT = re.findall(r"\b(\w+\s)(LTG\w+\s)(\w+\s)(\S+\s)(\w+\s)(\w+\s)(\S+)\b", metar)
-lightningOHD = re.findall(r"\b(\w+\s)(LTG\w+\s)(\w+\s)(\w+\s)(\w+\s)(\w+\s)(\S+)\b", metar)
-lightningVC = re.findall(r"\b(\w+\s)(LTG\w+\s)(\w+\s)(\S+\s)(\w+\s)(\w+\s)(\S+\s)(\w+\s)(\S+)\b", metar)
+directions = ['N','NE','E','SE','S','SW','W','NW','UNKN','ALQDS']
+ltg_errors = [0,0,0,0,0,0,0]
+            
+lightningDSNT = re.findall(r"\b(\w+\s)(LTG\w+\s)(\DSNT\s)(\S+\s(?:AND \w+\s)?)(\w+\s)(\w+\s)(\S+\s(?:AND \w+\s)?)\b", metarsplit[1])
+lightningOHD = re.findall(r"\b(\w+\s)(LTG\w+\s)(\OHD\s)(\w+\s)(\w+\s)(\w+\s)(\S+)\b", metarsplit[1])
+lightningVC = re.findall(r"\b(\w+\s)(LTG\w+\s)(\VC\s)(\S+\s(?:AND \w+\s)?)(\w+\s)(\w+\s)(\S+\s(?:AND \w+\s)?)(\w+\s)(\S+)\b", metarsplit[1])
 
 print(lightningDSNT)
 print(lightningOHD)
 print(lightningVC)
 
-#DSNT lighting[0] length should be 7
-if(len(lightningDSNT) > 0):
-    if lightningDSNT[0][2].strip() == 'DSNT':
-        for frequency in frequencies:
-            if frequency == lightningDSNT[0][0].strip():
-                print("frequency okay")
-        for direction in directions:
-            if direction in lightningDSNT[0][3].strip():
-                print("first direction okay")
-            if direction in lightningDSNT[0][6].strip():
-                print("second direction okay")
-        if lightningDSNT[0][4].strip() == 'CB':
-            print("CB position okay")
-        if lightningDSNT[0][5].strip() == 'DSNT':
-            print("second DSNT position okay")
+if "LTG" in metarsplit[1]:
+    #DSNT lighting[0] length should be 7
+    if(len(lightningDSNT) > 0):
+        if lightningDSNT[0][2].strip() == 'DSNT':
+            for frequency in frequencies:
+                if frequency == lightningDSNT[0][0].strip():                    
+                    ltg_errors[0] += 1
+            for direction in directions:
+                if direction in lightningDSNT[0][3].strip():                    
+                    ltg_errors[1] += 1
+                if direction in lightningDSNT[0][6].strip():                    
+                    ltg_errors[2] += 1
+            if lightningDSNT[0][4].strip() == 'CB':                
+                ltg_errors[3] += 1
+            if lightningDSNT[0][5].strip() == 'DSNT':                
+                ltg_errors[4] += 1
+        if ltg_errors[0] == 0 or ltg_errors[1] == 0 or ltg_errors[2] == 0 or ltg_errors[3] == 0 or ltg_errors[4] == 0:
+            print("Error in LTG remark " + metar)
+            print(ltg_errors)
+            ltg_errors = [0,0,0,0,0,0,0]
 
-#OHD lighting[0] length should be 7
-if(len(lightningOHD) > 0):
-    if lightningOHD[0][2].strip() == 'OHD':
-        for frequency in frequencies:
-            if frequency == lightningOHD[0][0].strip():
-                print("frequency okay")
-        for direction in directions:
-            if direction in lightningOHD[0][6].strip():
-                print("direction okay")
-        if lightningOHD[0][3].strip() == 'TS':
-            print("TS position okay")
-        if lightningOHD[0][4].strip() == 'OHD':
-            print("second OHD position okay")
-        if lightningOHD[0][5].strip() == 'MOV':
-            print("MOV position okay")
+    #OHD lighting[0] length should be 7
+    if(len(lightningOHD) > 0):
+        if lightningOHD[0][2].strip() == 'OHD':
+            for frequency in frequencies:
+                if frequency == lightningOHD[0][0].strip():
+                    ltg_errors[0] += 1
+            for direction in directions:
+                if direction in lightningOHD[0][6].strip():
+                    ltg_errors[1] += 1
+            if lightningOHD[0][3].strip() == 'TS':
+                ltg_errors[2] += 1
+            if lightningOHD[0][4].strip() == 'OHD':
+                ltg_errors[3] += 1
+            if lightningOHD[0][5].strip() == 'MOV':
+                ltg_errors[4] += 1
+        if ltg_errors[0] == 0 or ltg_errors[1] == 0 or ltg_errors[2] == 0 or ltg_errors[3] == 0 or ltg_errors[4] == 0:
+            print("Error in LTG remark " + metar)
+            print(ltg_errors)
+            ltg_errors = [0,0,0,0,0,0,0]
 
+    #VC lightning[0] should be length 9
+    if(len(lightningVC) > 0):
+        if lightningVC[0][2].strip() == 'VC': #strip whitespace after VC
+            for frequency in frequencies:
+                if frequency == lightningVC[0][0].strip():
+                    ltg_errors[0] += 1                    
+            for direction in directions:
+                if direction in lightningVC[0][3].strip():
+                    ltg_errors[1] += 1
+                if direction in lightningVC[0][6].strip():
+                    ltg_errors[2] += 1
+                if direction in lightningVC[0][8].strip():
+                    ltg_errors[3] += 1                
+            if lightningVC[0][4].strip() == 'TS':
+                ltg_errors[4] += 1
+            if lightningVC[0][5].strip() == 'VC':
+                ltg_errors[5] += 1
+            if lightningVC[0][7].strip() == 'MOV':
+                ltg_errors[6] += 1            
+        if ltg_errors[0] == 0 or ltg_errors[1] == 0 or ltg_errors[2] == 0 or ltg_errors[3] == 0 or ltg_errors[4] == 0 or ltg_errors[5] == 0 or ltg_errors[6] == 0:
+            print("Error in LTG remark " + metar)
+            print(ltg_errors)
+            ltg_errors = [0,0,0,0,0,0,0]
 
-#VC lightning[0] should be length 9
-if(len(lightningVC) > 0):
-    if lightningVC[0][2].strip() == 'VC': #strip whitespace after VC
-        for frequency in frequencies:
-            if frequency == lightningVC[0][0].strip():
-                print("frequency okay")
-                
-        for direction in directions:
-            if direction in lightningVC[0][3].strip():
-                print("first direction okay")
-            if direction in lightningVC[0][6].strip():
-                print("second direction okay")
-            if direction in lightningVC[0][8].strip():
-                print("third direction okay")
-            
-        if lightningVC[0][4].strip() == 'TS':
-            print("TS position okay")
-
-        if lightningVC[0][5].strip() == 'VC':
-            print("VC position okay")
-
-        if lightningVC[0][7].strip() == 'MOV':
-            print("MOV position okay")
-
+if "LTG" in metarsplit[1]:
+    if len(lightningDSNT) == 0 and len(lightningOHD) == 0 and len(lightningVC) == 0:
+        print("Error in lightning remarks " + metar)
